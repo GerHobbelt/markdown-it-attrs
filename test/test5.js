d78ba538a2ce2092c03b2851c5598fb049c37bdc
Ger Hobbelt <ger@hobbelt.com>
2020-07-03 22:27:04 +0200

    updated npm packages

--- test/test5.js
+++ test/test5.js
@@ -6391,6 +6391,10 @@ var entities$1 = /*#__PURE__*/Object.freeze({
 
 var require$$0 = getCjsExportFromNamespace(entities$1);
 
+// HTML5 entities map: { name -> utf16string }
+//
+
+
 /*eslint quotes:0*/
 var entities$2 = require$$0;
 
@@ -6959,6 +6963,9 @@ var uc_micro = {
 };
 
 var utils = createCommonjsModule(function (module, exports) {
+// Utilities
+//
+
 
 function isNil(v) { return v === null || typeof v === 'undefined'; }
 
@@ -7301,6 +7308,11 @@ exports.trimLeftOffset      = trimLeftOffset;
 });
 
 // Parse link label
+//
+// this function assumes that first character ("[") already matches;
+// returns the end of the label
+//
+
 
 var parse_link_label = function parseLinkLabel(state, start, disableNested) {
   let level, found, marker, prevPos,
@@ -7343,6 +7355,11 @@ var parse_link_label = function parseLinkLabel(state, start, disableNested) {
 
   return labelEnd;
 };
+
+// Parse link destination
+//
+
+
 
 let unescapeAll = utils.unescapeAll;
 
@@ -7419,6 +7436,11 @@ var parse_link_destination = function parseLinkDestination(str, pos, max) {
   return result;
 };
 
+// Parse link title
+//
+
+
+
 let unescapeAll$1 = utils.unescapeAll;
 
 
@@ -7468,6 +7490,10 @@ var parse_link_title = function parseLinkTitle(str, pos, max) {
   return result;
 };
 
+// Just a shortcut for bulk export
+
+
+
 var parseLinkLabel       = parse_link_label;
 var parseLinkDestination = parse_link_destination;
 var parseLinkTitle       = parse_link_title;
@@ -7477,6 +7503,16 @@ var helpers = {
 	parseLinkDestination: parseLinkDestination,
 	parseLinkTitle: parseLinkTitle
 };
+
+/**
+ * class Renderer
+ *
+ * Generates HTML from parsed token stream. Each instance has independent
+ * copy of rules. Those can be rewritten with ease. Also, you can add new
+ * rules if you create plugin and adds new token types.
+ **/
+
+
 
 let assign          = utils.assign;
 let unescapeAll$2     = utils.unescapeAll;
@@ -7835,6 +7871,7 @@ var renderer = Renderer;
  **/
 
 
+
 /**
  * new Ruler()
  **/
@@ -8168,6 +8205,8 @@ var ruler = Ruler;
 // Normalize input string
 
 
+
+
 // https://spec.commonmark.org/0.29/#line-ending
 let NEWLINES_RE  = /\r\n?|\n/g;
 let NULL_RE      = /\0/g;
@@ -8225,6 +8264,19 @@ var inline = function inline(state, positionOffset) {
 };
 
 // Simple typographic replacements
+//
+// (c) (C) → ©
+// (tm) (TM) → ™
+// (r) (R) → ®
+// +- → ±
+// (p) (P) -> §
+// ... → … (also ?.... → ?.., !.... → !..)
+// ???????? → ???, !!!!! → !!!, `,,` → `,`
+// -- → &ndash;, --- → &mdash;
+// --> → →; <-- → ←; <--> → ↔
+// ==> → ⇒; <== → ⇐; <==> → ⇔
+//
+
 
 // TODO:
 // - fractionals 1/2, 1/4, 3/4 -> ½, ¼, ¾
@@ -8346,6 +8398,11 @@ var replacements = function replace(state) {
 
   }
 };
+
+// Convert straight quotation marks to typographic ones
+//
+
+
 
 let isWhiteSpace   = utils.isWhiteSpace;
 let isPunctChar    = utils.isPunctChar;
@@ -8545,6 +8602,8 @@ var smartquotes = function smartquotes(state) {
 };
 
 // Token class
+
+
 
 
 /**
@@ -8777,6 +8836,13 @@ Token.prototype.clone = function clone() {
 
 var token = Token;
 
+// Core state object
+//
+
+
+
+
+
 function StateCore(src, md, env) {
   this.src = src;
   this.env = env;
@@ -8790,6 +8856,18 @@ StateCore.prototype.Token = token;
 
 
 var state_core = StateCore;
+
+/** internal
+ * class Core
+ *
+ * Top-level rules executor. Glues block/inline parsers and does intermediate
+ * transformations.
+ **/
+
+
+
+
+
 
 let _rules = [
   [ 'normalize',      normalize      ],
@@ -8836,6 +8914,10 @@ Core.prototype.State = state_core;
 
 
 var parser_core = Core;
+
+// GFM table, non-standard
+
+
 
 let isSpace = utils.isSpace;
 let trimLeftOffset = utils.trimLeftOffset;
@@ -9098,6 +9180,8 @@ var table = function table(state, startLine, endLine, silent) {
 // Code block (4 spaces padded)
 
 
+
+
 var code = function code(state, startLine, endLine/*, silent*/) {
   let nextLine, last, token,
       pos = state.bMarks[startLine],
@@ -9134,6 +9218,8 @@ var code = function code(state, startLine, endLine/*, silent*/) {
 };
 
 // fences (``` lang, ~~~ lang)
+
+
 
 
 var fence = function fence(state, startLine, endLine, silent) {
@@ -9232,6 +9318,10 @@ var fence = function fence(state, startLine, endLine, silent) {
 
   return true;
 };
+
+// Block quotes
+
+
 
 let isSpace$1 = utils.isSpace;
 
@@ -9515,6 +9605,10 @@ var blockquote = function blockquote(state, startLine, endLine, silent) {
   return true;
 };
 
+// Horizontal rule
+
+
+
 let isSpace$2 = utils.isSpace;
 
 
@@ -9559,6 +9653,10 @@ var hr = function hr(state, startLine, endLine, silent) {
 
   return true;
 };
+
+// Lists
+
+
 
 let isSpace$3 = utils.isSpace;
 
@@ -10114,6 +10212,10 @@ var reference = function reference(state, startLine, _endLine, silent) {
   return true;
 };
 
+// heading (#, ##, ...)
+
+
+
 let isSpace$5 = utils.isSpace;
 let trimLeftOffset$1 = utils.trimLeftOffset;
 
@@ -10175,6 +10277,10 @@ var heading = function heading(state, startLine, endLine, silent) {
 
   return true;
 };
+
+// lheading (---, ===)
+
+
 
 let trimLeftOffset$2 = utils.trimLeftOffset;
 
@@ -10265,6 +10371,9 @@ var lheading = function lheading(state, startLine, endLine/*, silent*/) {
 };
 
 // List of valid html blocks names, according to commonmark spec
+// http://jgm.github.io/CommonMark/spec.html#html-blocks
+
+
 
 
 var html_blocks = [
@@ -10335,6 +10444,8 @@ var html_blocks = [
 
 // Regexps to match html elements
 
+
+
 let attr_name     = '[a-zA-Z_:@][a-zA-Z0-9:._-]*';
 
 let unquoted      = '[^"\'=<>`\\x00-\\x20]+';
@@ -10364,6 +10475,12 @@ var html_re = {
 	HTML_TAG_RE: HTML_TAG_RE_1,
 	HTML_OPEN_CLOSE_TAG_RE: HTML_OPEN_CLOSE_TAG_RE_1
 };
+
+// HTML block
+
+
+
+
 
 let HTML_OPEN_CLOSE_TAG_RE$1 = html_re.HTML_OPEN_CLOSE_TAG_RE;
 
@@ -10434,6 +10551,10 @@ var html_block = function html_block(state, startLine, endLine, silent) {
   return true;
 };
 
+// Paragraph
+
+
+
 let trimLeftOffset$3 = utils.trimLeftOffset;
 
 
@@ -10491,6 +10612,11 @@ var paragraph = function paragraph(state, startLine/*, endLine*/) {
 
   return true;
 };
+
+// Parser state class
+
+
+
 
 let isSpace$6 = utils.isSpace;
 
@@ -10724,6 +10850,17 @@ StateBlock.prototype.Token = token;
 
 var state_block = StateBlock;
 
+/** internal
+ * class ParserBlock
+ *
+ * Block-level tokenizer.
+ **/
+
+
+
+
+
+
 let _rules$1 = [
   // First 2 params - rule name & source. Secondary array - list of rules,
   // which can be terminated by this one.
@@ -10837,6 +10974,9 @@ ParserBlock.prototype.State = state_block;
 var parser_block = ParserBlock;
 
 // Handle implicit links found by rules_core/linkify that were not yet
+// subsumed by other inline rules (backticks, link, etc.)
+
+
 
 var tokenize = function linkify(state, silent) {
   let link, url, fullUrl, urlText, token;
@@ -10954,6 +11094,9 @@ var linkify = {
 };
 
 // Skip text characters for text token, place those to pending buffer
+// and increment current pos
+
+
 
 
 // Rule to skip pure text
@@ -10987,6 +11130,7 @@ function isTerminatorChar(ch) {
   case 0x5F/* _ */:
   case 0x60/* ` */:
   case 0x7B/* { */:
+  case 0x7C/* | */:
   case 0x7D/* } */:
   case 0x7E/* ~ */:
     return true;
@@ -10998,9 +11142,17 @@ function isTerminatorChar(ch) {
 var text = function text(state, silent) {
   let pos = state.pos;
 
-  while (pos < state.posMax && !isTerminatorChar(state.src.charCodeAt(pos)) &&
-         (!state.links || !state.links[pos])) {
-    pos++;
+  let terminatorRe = state.md.options.inlineTokenTerminatorsRe;
+  if (!terminatorRe) {
+    while (pos < state.posMax && !isTerminatorChar(state.src.charCodeAt(pos)) &&
+           (!state.links || !state.links[pos])) {
+      pos++;
+    }
+  } else {
+    while (pos < state.posMax && !isTerminatorChar(state.src.charCodeAt(pos)) && !terminatorRe.test(state.src[pos]) &&
+           (!state.links || !state.links[pos])) {
+      pos++;
+    }
   }
 
   if (pos === state.pos) { return false; }
@@ -11011,6 +11163,10 @@ var text = function text(state, silent) {
 
   return true;
 };
+
+// Proceess '\n'
+
+
 
 let isSpace$7 = utils.isSpace;
 
@@ -11049,6 +11205,10 @@ var newline = function newline(state, silent) {
   state.pos = pos;
   return true;
 };
+
+// Process escaped chars and hardbreaks
+
+
 
 let isSpace$8 = utils.isSpace;
 
@@ -11101,6 +11261,8 @@ var _escape = function escape(state, silent) {
 
 // Parse backticks
 
+
+
 var backticks = function backtick(state, silent) {
   let start, max, marker, matchStart, matchEnd, token,
       pos = state.pos,
@@ -11143,6 +11305,10 @@ var backticks = function backtick(state, silent) {
   state.pos += marker.length;
   return true;
 };
+
+// ~~strike through~~
+//
+
 
 let getLineOffset  = utils.getLineOffset;
 
@@ -11279,6 +11445,10 @@ var strikethrough = {
 	tokenize: tokenize$1,
 	postProcess: postProcess_1
 };
+
+// Process *this* and _that_
+//
+
 
 let getLineOffset$1  = utils.getLineOffset;
 
@@ -11423,6 +11593,10 @@ var emphasis = {
 	tokenize: tokenize$2,
 	postProcess: postProcess_1$1
 };
+
+// Process [link](<to> "stuff")
+
+
 
 let normalizeReference$1   = utils.normalizeReference;
 let isSpace$9              = utils.isSpace;
@@ -11570,6 +11744,10 @@ var link = function link(state, silent) {
   state.posMax = max;
   return true;
 };
+
+// Process ![image](<src> "title")
+
+
 
 let normalizeReference$2   = utils.normalizeReference;
 let isSpace$a              = utils.isSpace;
@@ -11735,6 +11913,8 @@ var image$1 = function image(state, silent) {
 // Process autolinks '<protocol:...>'
 
 
+
+
 /*eslint max-len:0*/
 let EMAIL_RE    = /^<([a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*)>/;
 let AUTOLINK_RE = /^<([a-zA-Z][a-zA-Z0-9+.\-]{1,31}):([^<>\x00-\x20]*)>/;
@@ -11807,6 +11987,11 @@ var autolink = function autolink(state, silent) {
   return false;
 };
 
+// Process html tags
+
+
+
+
 let HTML_TAG_RE$1 = html_re.HTML_TAG_RE;
 
 
@@ -11849,6 +12034,11 @@ var html_inline = function html_inline(state, silent) {
   state.pos += match[0].length;
   return true;
 };
+
+// Process html entity - &#123;, &#xAF;, &quot;, ...
+
+
+
 
 let has               = utils.has;
 let isValidEntityCode = utils.isValidEntityCode;
@@ -11895,6 +12085,8 @@ var entity = function entity(state, silent) {
 };
 
 // For each opening emphasis-like marker find a matching closing one
+//
+
 
 
 function processDelimiters(state, delimiters) {
@@ -12002,6 +12194,14 @@ var balance_pairs = function link_pairs(state) {
 };
 
 // Clean up tokens after emphasis and strikethrough postprocessing:
+// merge adjacent text nodes into one and re-calculate all token levels
+//
+// This is necessary because initially emphasis delimiter markers (*, _, ~)
+// are treated as their own separate text tokens. Then emphasis rule either
+// leaves them as text (needed to merge with adjacent text) or turns them
+// into opening/closing tags (which messes up levels inside).
+//
+
 
 
 var text_collapse = function text_collapse(state) {
@@ -12041,6 +12241,12 @@ var text_collapse = function text_collapse(state) {
     tokens.length = last;
   }
 };
+
+// Inline parser state
+
+
+
+
 
 let isWhiteSpace$1   = utils.isWhiteSpace;
 let isPunctChar$1    = utils.isPunctChar;
@@ -12192,6 +12398,17 @@ StateInline.prototype.Token = token;
 
 
 var state_inline = StateInline;
+
+/** internal
+ * class ParserInline
+ *
+ * Tokenizes paragraph content.
+ **/
+
+
+
+
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // Parser rules
@@ -13637,6 +13854,8 @@ const punycode = {
 // markdown-it default options
 
 
+
+
 var _default = {
   options: {
     html:         false,        // Enable HTML tags in source
@@ -13675,11 +13894,22 @@ var _default = {
     //
     highlight: null,
 
-    maxNesting:   100            // Internal protection, recursion limit
+    // A regex which matches *additional* characters in an inline text string which may serve
+    // as the start of a special word, i.e. the start of anything that might be matched
+    // by a markdown-it parse rule / plugin.
+    //
+    // Using this option will slow markdown-it, hence you should only use it when you need it,
+    // e.g. when writing custom plugins which are not looking for words which start with one
+    // of the default set of sentinel characters as specified in rules_inline/text.js:
+    //
+    // !, ", #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, or ~
+    inlineTokenTerminatorsRe: null,
+
+    // Internal protection, recursion limit
+    maxNesting: 100
   },
 
   components: {
-
     core: {},
     block: {},
     inline: {}
@@ -13687,6 +13917,9 @@ var _default = {
 };
 
 // "Zero" preset, with nothing enabled. Useful for manual configuring of simple
+// modes. For example, to parse bold/italic only.
+
+
 
 
 var zero = {
@@ -13727,11 +13960,22 @@ var zero = {
     //
     highlight: null,
 
-    maxNesting:   20            // Internal protection, recursion limit
+    // A regex which matches *additional* characters in an inline text string which may serve
+    // as the start of a special word, i.e. the start of anything that might be matched
+    // by a markdown-it parse rule / plugin.
+    //
+    // Using this option will slow markdown-it, hence you should only use it when you need it,
+    // e.g. when writing custom plugins which are not looking for words which start with one
+    // of the default set of sentinel characters as specified in rules_inline/text.js:
+    //
+    // !, ", #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, or ~
+    inlineTokenTerminatorsRe: null,
+
+    // Internal protection, recursion limit
+    maxNesting: 20
   },
 
   components: {
-
     core: {
       rules: [
         'normalize',
@@ -13759,6 +14003,8 @@ var zero = {
 };
 
 // Commonmark default options
+
+
 
 
 var commonmark = {
@@ -13799,11 +14045,22 @@ var commonmark = {
     //
     highlight: null,
 
-    maxNesting:   20            // Internal protection, recursion limit
+    // A regex which matches *additional* characters in an inline text string which may serve
+    // as the start of a special word, i.e. the start of anything that might be matched
+    // by a markdown-it parse rule / plugin.
+    //
+    // Using this option will slow markdown-it, hence you should only use it when you need it,
+    // e.g. when writing custom plugins which are not looking for words which start with one
+    // of the default set of sentinel characters as specified in rules_inline/text.js:
+    //
+    // !, ", #, $, %, &, ', (, ), *, +, ,, -, ., /, :, ;, <, =, >, ?, @, [, \, ], ^, _, `, {, |, }, or ~
+    inlineTokenTerminatorsRe: null,
+
+    // Internal protection, recursion limit
+    maxNesting: 20
   },
 
   components: {
-
     core: {
       rules: [
         'normalize',
@@ -13848,6 +14105,22 @@ var commonmark = {
     }
   }
 };
+
+// Main parser class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let config = {
   'default': _default,
